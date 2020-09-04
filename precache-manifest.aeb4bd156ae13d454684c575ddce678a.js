fe8f311e4f9b3cb8494d2557e42ce9a7932bc5b5
ankushydv26 <tankush027@gmail.com>
2020-09-05 03:11:40 +0530

    Updates

---
+++ precache-manifest.aeb4bd156ae13d454684c575ddce678a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4835e9e00a7c795e578a88acad6ad8c8",
+    "url": "/WebSite/index.html"
+  },
+  {
+    "revision": "135a5ce31a9eef084a9e",
+    "url": "/WebSite/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "343a4a0ec5792edb56b6",
+    "url": "/WebSite/static/css/main.c3e49089.chunk.css"
+  },
+  {
+    "revision": "135a5ce31a9eef084a9e",
+    "url": "/WebSite/static/js/2.a727aa8d.chunk.js"
+  },
+  {
+    "revision": "cd8eb1d4f486ae91760f720fdd44986d",
+    "url": "/WebSite/static/js/2.a727aa8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343a4a0ec5792edb56b6",
+    "url": "/WebSite/static/js/main.fa610810.chunk.js"
+  },
+  {
+    "revision": "60b39dea2c70008b3a69",
+    "url": "/WebSite/static/js/runtime-main.1f527ec6.js"
+  }
+]);
